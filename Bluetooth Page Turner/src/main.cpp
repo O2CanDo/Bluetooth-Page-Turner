7a00b7635f2a21dd5e836328a845f51ba983f44a
Luca Straub <Luca.Straub@bwedu.de>
2024-10-31 02:21:10 +0100

    Basic switch implementation

--- Bluetooth Page Turner/src/main.cpp
+++ Bluetooth Page Turner/src/main.cpp
@@ -2,18 +2,42 @@
 #include <Arduino.h>
 #include <BleKeyboard.h>
 
-// to do: define pins
+
+const int INPUT_LEFT = 16;
+const int INPUT_RIGHT = 17;
+
 
 BleKeyboard blePageTurner;
 
+
+
 void setup() {
+
+  pinMode(INPUT_LEFT, INPUT_PULLUP);
+  pinMode(INPUT_RIGHT, INPUT_PULLUP);
+
   Serial.begin(115200);
   blePageTurner.begin();
-  Serial.println("Setup successful.");
+  Serial.println("\nSetup successful.");
 }
 
 void loop() {
   
+  if(blePageTurner.isConnected()) {
+    Serial.println("connection successful");
+
+    if(digitalRead(INPUT_LEFT == LOW)){
+      Serial.println("left pressed");
+      blePageTurner.write(KEY_LEFT_ARROW);
+    }
+    if (digitalRead(INPUT_LEFT == LOW)) {
+      Serial.println("right pressed");
+      blePageTurner.write(KEY_RIGHT_ARROW);
+    }
+  }
+  delay(100);
 }
+  
+
 
 
