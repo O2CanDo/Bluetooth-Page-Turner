56cd2ce2069b9d070a70abe819f23436913a27fc
Luca Straub <Luca.Straub@bwedu.de>
2024-10-31 02:36:58 +0100

    Fixed simultaneous trigger of left and right signal

--- Bluetooth Page Turner/src/main.cpp
+++ Bluetooth Page Turner/src/main.cpp
@@ -1,7 +1,8 @@
 
+
+
 #include <Arduino.h>
 #include <BleKeyboard.h>
-
 
 const int INPUT_LEFT = 16;
 const int INPUT_RIGHT = 17;
@@ -12,13 +13,13 @@ BleKeyboard blePageTurner;
 
 
 void setup() {
-
   pinMode(INPUT_LEFT, INPUT_PULLUP);
   pinMode(INPUT_RIGHT, INPUT_PULLUP);
 
   Serial.begin(115200);
   blePageTurner.begin();
-  Serial.println("\nSetup successful.");
+  Serial.print("\nSetup successful for version: V1 ");
+ 
 }
 
 void loop() {
@@ -26,16 +27,16 @@ void loop() {
   if(blePageTurner.isConnected()) {
     Serial.println("connection successful");
 
-    if(digitalRead(INPUT_LEFT == LOW)){
+    if(digitalRead(INPUT_LEFT) == LOW){
       Serial.println("left pressed");
       blePageTurner.write(KEY_LEFT_ARROW);
     }
-    if (digitalRead(INPUT_LEFT == LOW)) {
+    if (digitalRead(INPUT_RIGHT) == LOW) {
       Serial.println("right pressed");
       blePageTurner.write(KEY_RIGHT_ARROW);
     }
   }
-  delay(100);
+  delay(500);
 }
   
 
